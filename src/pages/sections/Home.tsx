7c3490b2c57169f260527bc00f45d81ea487f6ca
Sohan Gujari <103947095+sohangujari@users.noreply.github.com>
2024-07-22 12:39:19 +0530

    Background Video

--- src/pages/sections/Home.tsx
+++ src/pages/sections/Home.tsx
@@ -91,7 +91,7 @@ function Home() {
             left: 0,
             width: "100%",
             height: "100%",
-            backgroundColor: "rgba(0, 0, 0, 0.2)",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
             zIndex: "0",
           }}
         />
