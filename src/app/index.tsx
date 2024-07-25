163fb92f4869176e1105152dc8b1becd63e34cc3
Sohan Gujari <103947095+sohangujari@users.noreply.github.com>
2024-07-25 21:50:58 +0530

    Change base font to open sans

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,6 +10,13 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/700.css";
 import "@fontsource/roboto/900.css";
 
+import '@fontsource/open-sans/300.css';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
+import '@fontsource/open-sans/800.css';
+
 // Styles
 import "../assets/css/setup.css";
 import "../assets/css/sm-clean.css";
